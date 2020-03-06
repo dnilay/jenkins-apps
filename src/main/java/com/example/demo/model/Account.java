373fdd66653334d647cf177deb7dd10864009e6d
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2020-03-06 13:28:05 +0530

    commit-2

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,10 +1,18 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Account {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer accountNumber;
 	private String accountHolderName;
 	private Double initialBalance;
